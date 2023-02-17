2aef36cec11f7aa56dd728b283245df1d191f29c
inyoung <shoush4916@gmail.com>
2023-02-17 12:42:55 +0900

    test

--- src/main/java/com/side/fitflow/user/entity/User.java
+++ src/main/java/com/side/fitflow/user/entity/User.java
@@ -1,4 +1,11 @@
 package com.side.fitflow.user.entity;
 
+import javax.persistence.*;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="user_id")
+    private Long userId;
 }
--- src/main/java/com/side/fitflow/user/repository/UserRepository.java
+++ src/main/java/com/side/fitflow/user/repository/UserRepository.java
@@ -1,4 +1,9 @@
 package com.side.fitflow.user.repository;
 
-public class UserRepository {
+import com.side.fitflow.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
 }
