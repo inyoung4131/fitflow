d5d9c6a03750f9fbb7a62af4c32ff0b729d2cbce
inyoung <shoush4916@gmail.com>
2023-02-17 15:26:58 +0900

    t

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -17,7 +17,7 @@ if [ -z "$CURRENT_PID" ]; then
     echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
     echo "> kill -15 $CURRENT_PID"
-    kill -15 $CURRENT_PID
+    sudo kill -15 $CURRENT_PID
     sleep 10
 fi
 
@@ -29,8 +29,10 @@ echo "> JAR Name: $JAR_NAME"
 
 echo "> $JAR_NAME 에 실행권한 추가"
 
-chmod +x $JAR_NAME
+sudo chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
-nohup java -jar $JAR_NAME &
+sudo nohup java -jar \
+         -Dspring.config.location=classpath:/application.properties \
+         $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
